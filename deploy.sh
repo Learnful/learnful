2b7bd7b938e0c5d504abede1ad9e50b18dff759e
Piotr Kaminski <piotr@ideanest.com>
2014-05-19 23:13:51 -0700

    Fix build script.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 
-echo $BRANCH
-echo $PULL_REQUEST
+if [[ "$BRANCH" = "master" && -z "$PULL_REQUEST" ]]; then
+  echo "Deploying to production..."
+  cd client
+  printenv AWS_CREDENTIALS >aws-credentials.json
+  s3-upload
+else
+  echo "Not deploying"
+fi
